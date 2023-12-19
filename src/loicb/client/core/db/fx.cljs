54fce926ea0586d700a94686711ed803b32af562
skydread1 <loic.blanchard01@gmail.com>
2023-12-20 00:40:34 +0800

    Add code block syntax highlighting

--- src/loicb/client/core/db/fx.cljs
+++ src/loicb/client/core/db/fx.cljs
@@ -1,7 +1,19 @@
 (ns loicb.client.core.db.fx
-  (:require [loicb.client.core.db.class-utils :as cu]
-            [loicb.client.core.db.localstorage :as l-storage]
+  (:require [cljsjs.highlight]
+            [cljsjs.highlight.langs.asciidoc]
+            [cljsjs.highlight.langs.bash]
+            [cljsjs.highlight.langs.clojure]
+            [cljsjs.highlight.langs.clojure-repl]
+            [cljsjs.highlight.langs.cpp]
+            [cljsjs.highlight.langs.dockerfile]
+            [cljsjs.highlight.langs.java]
+            [cljsjs.highlight.langs.javascript]
+            [cljsjs.highlight.langs.lisp]
+            [cljsjs.highlight.langs.markdown]
+            [cljsjs.highlight.langs.python]
             [clojure.edn :as edn]
+            [loicb.client.core.db.class-utils :as cu]
+            [loicb.client.core.db.localstorage :as l-storage]
             [re-frame.core :as rf]
             [reagent.core :as reagent]))
 
@@ -30,6 +42,16 @@
    (reagent/after-render #(let [el (or (.getElementById js/document fragment)
                                        (.getElementById js/document "app"))]
                             (.scrollIntoView el)))))
+
+;; ---------- Syntax Highlighting ----------
+
+(rf/reg-fx
+ :fx.app/highlight-code
+ (fn [id]
+   (reagent/after-render
+    #(do (.configure js/hljs #js {:cssSelector (str "#" id " pre code")
+                                  :ignoreUnescapedHTML true})
+         (.highlightAll js/hljs)))))
 
 ;; ---------- Local Storage ----------
 
